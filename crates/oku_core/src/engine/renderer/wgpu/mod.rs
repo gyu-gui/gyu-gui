c024bdfccb9cad34ef70502fc82f19b76e6336a2
NoahR02 <noahreppert95@gmail.com>
2024-10-04 16:27:18 -0400

    Organize the wgpu code some more

--- crates/oku_core/src/engine/renderer/wgpu/mod.rs
+++ crates/oku_core/src/engine/renderer/wgpu/mod.rs
@@ -2,15 +2,17 @@ mod texture;
 mod context;
 mod uniform;
 mod pipeline_2d;
+mod vertex;
+mod camera;
 
 use crate::engine::renderer::color::Color;
 use crate::engine::renderer::renderer::{Rectangle, Renderer};
 use glam;
 use image::{GenericImageView, ImageEncoder};
-use std::collections::HashMap;
 use std::sync::Arc;
 use wgpu::util::DeviceExt;
 use winit::window::Window;
+use crate::engine::renderer::wgpu::camera::Camera;
 use crate::engine::renderer::wgpu::context::{create_surface_config, request_adapter, request_device_and_queue, Context};
 use crate::engine::renderer::wgpu::pipeline_2d::Pipeline2D;
 use crate::engine::renderer::wgpu::texture::Texture;
@@ -18,33 +20,6 @@ use crate::engine::renderer::wgpu::texture::Texture;
 pub struct WgpuRenderer<'a> {
     context: Context<'a>,
     pipeline2d: Pipeline2D,
-}
-
-struct RectangleBatch {
-    texture_path: Option<String>,
-    rectangle_vertices: Vec<Vertex>,
-    rectangle_indices: Vec<u32>,
-}
-
-fn bind_group_from_2d_texture(
-    device: &wgpu::Device,
-    texture_bind_group_layout: &wgpu::BindGroupLayout,
-    texture: &Texture,
-) -> wgpu::BindGroup {
-    device.create_bind_group(&wgpu::BindGroupDescriptor {
-        layout: texture_bind_group_layout,
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: wgpu::BindingResource::TextureView(&texture.view),
-            },
-            wgpu::BindGroupEntry {
-                binding: 1,
-                resource: wgpu::BindingResource::Sampler(&texture.sampler),
-            },
-        ],
-        label: Some("oku_bind_group"),
-    })
 }
 
 impl<'a> WgpuRenderer<'a> {
@@ -126,72 +101,4 @@ impl Renderer for WgpuRenderer<'_> {
     fn submit(&mut self) {
         self.pipeline2d.submit(&mut self.context)
     }     
-}
-
-#[repr(C)]
-#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
-struct Vertex {
-    position: [f32; 3],
-    color: [f32; 4],
-    tex_coords: [f32; 2],
-}
-
-struct Camera {
-    width: f32,
-    height: f32,
-    z_near: f32,
-    z_far: f32,
-}
-
-#[repr(C)]
-#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-struct CameraUniform {
-    view_proj: [[f32; 4]; 4],
-}
-
-impl CameraUniform {
-    fn new() -> Self {
-        Self {
-            view_proj: glam::Mat4::IDENTITY.to_cols_array_2d(),
-        }
-    }
-
-    fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_view_projection_matrix().to_cols_array_2d();
-    }
-}
-
-impl Camera {
-    fn build_view_projection_matrix(&self) -> glam::Mat4 {
-        let view = glam::Mat4::IDENTITY;
-        let proj = glam::Mat4::orthographic_lh(0.0, self.width, self.height, 0.0, self.z_near, self.z_far);
-        proj * view
-    }
-}
-
-impl Vertex {
-    fn description<'a>() -> wgpu::VertexBufferLayout<'a> {
-        use std::mem;
-        wgpu::VertexBufferLayout {
-            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
-            step_mode: wgpu::VertexStepMode::Vertex,
-            attributes: &[
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    shader_location: 0,
-                    format: wgpu::VertexFormat::Float32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
-                    shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x4,
-                },
-                wgpu::VertexAttribute {
-                    offset: mem::size_of::<[f32; 7]>() as wgpu::BufferAddress,
-                    shader_location: 2,
-                    format: wgpu::VertexFormat::Float32x2,
-                },
-            ],
-        }
-    }
 }
--- crates/oku_core/src/engine/renderer/wgpu/pipeline_2d.rs
+++ crates/oku_core/src/engine/renderer/wgpu/pipeline_2d.rs
@@ -2,11 +2,33 @@ use std::collections::HashMap;
 use wgpu::util::DeviceExt;
 use crate::engine::renderer::color::Color;
 use crate::engine::renderer::renderer::Rectangle;
+use crate::engine::renderer::wgpu::camera::{Camera, CameraUniform};
 use crate::engine::renderer::wgpu::context::Context;
 use crate::engine::renderer::wgpu::texture::Texture;
-use crate::engine::renderer::wgpu::{bind_group_from_2d_texture, Camera, CameraUniform, Vertex};
+use crate::engine::renderer::wgpu::vertex::Vertex;
 
-struct RectangleBatch {
+fn bind_group_from_2d_texture(
+    device: &wgpu::Device,
+    texture_bind_group_layout: &wgpu::BindGroupLayout,
+    texture: &Texture,
+) -> wgpu::BindGroup {
+    device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: texture_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&texture.view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::Sampler(&texture.sampler),
+            },
+        ],
+        label: Some("oku_bind_group"),
+    })
+}
+
+pub struct RectangleBatch {
     texture_path: Option<String>,
     rectangle_vertices: Vec<Vertex>,
     rectangle_indices: Vec<u32>,
